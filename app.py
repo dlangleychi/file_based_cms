509426fbfee8e40f9aa58ccf3ba2a78363279c73
Daniel Langley <dlangley812@hotmail.com>
2026-03-17 00:41:24 -0500

    Complete Step 4

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+# app.secret_key='secret1'
+
+@app.route('/')
+def index():
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, "cms", "data")
+    files = [os.path.basename(path) for path in os.listdir(data_dir)]
+    return render_template('index.html', files=files)
+
+@app.route('/<filename>')
+def file_contents(filename):
+    root = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(root, "cms", "data")
+    return send_from_directory(data_dir, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5003)
+

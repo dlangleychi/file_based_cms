9ffac7eb4c0db55b69be0798dbffe286938674c9
Daniel Langley <dlangley812@hotmail.com>
2026-03-18 00:24:25 -0500

    Complete Step 6

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     root = os.path.abspath(os.path.dirname(__file__))
-    data_dir = os.path.join(root, "cms", "data")
+    data_dir = os.path.join(root, "file_based_cms", "data")
     files = [os.path.basename(path) for path in os.listdir(data_dir)]
     return render_template('index.html', files=files)
 
 @app.route('/<filename>')
 def file_contents(filename):
     root = os.path.abspath(os.path.dirname(__file__))
-    data_dir = os.path.join(root, "cms", "data")
+    data_dir = os.path.join(root, "file_based_cms", "data")
     return send_from_directory(data_dir, filename)
 
 if __name__ == '__main__':
